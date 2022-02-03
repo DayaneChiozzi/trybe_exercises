2be45ef3a1de7f90870b1eeb8bdaaef0b0d45526
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-03 18:46:40 -0300

    impresso todos os valores usando o console.log- e1

--- fundamentos/bloco4/dia2-JavaScript-array-e-loop-for/javaScript.js
+++ fundamentos/bloco4/dia2-JavaScript-array-e-loop-for/javaScript.js
@@ -7,3 +7,5 @@ let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for (let lista of names) {
     console.log(lista);
 }
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+console.log(numbers);
