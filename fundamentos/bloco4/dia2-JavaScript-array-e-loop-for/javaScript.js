7b1f83c8271943968b8769091632885044d4fec4
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-03 21:07:05 -0300

    numero maior do array-e5

--- fundamentos/bloco4/dia2-JavaScript-array-e-loop-for/javaScript.js
+++ fundamentos/bloco4/dia2-JavaScript-array-e-loop-for/javaScript.js
@@ -7,18 +7,32 @@ let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for (let lista of names) {
   console.log(lista);
 }
+
+// exercicio 1
 let total = 0;
 let media = 0;
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+// exercicio 2
 for (let index = 0; index < numbers.length; index += 1) {
   total += numbers[index];
 }
 
+// exercicio 3
 media = total / numbers.length;
 
+// exercicio 4
 if (media > 20) {
-console.log("Valor maior que 20.");
+  console.log("Valor maior que 20.");
 } else {
-  console.log( " Valor menor ou igual a 20")
+  console.log(" Valor menor ou igual a 20")
 }
+
+// exercicio 5
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > maiorNumero ) {
+    maiorNumero = numbers[index]
+  }
+}
+console.log(maiorNumero);
