e0f3ed062897b150a442489329ddd5ebe440e7c2
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-07 18:04:54 -0300

    Alterado Olá para Oi, testando Ctrl+D

---
+++ fundamentos/bloco4/dia4-objetos-e-funcoes/function.js
@@ -0,0 +1,47 @@
+function prinGreeting() {
+    console.log("olá");
+    // return => retorno dessa função
+}
+
+// printGreeting(); => chamada da função
+
+//Adicione seu nome na função.
+
+function printGreeting(name) {
+    console.log("olá", + name + " tudo bem com você?");
+
+}
+
+let primeiroNome = "Dayane";
+printGreeting(primeiroNome);
+
+function makeGreeting(name) {
+    return " olá, " + name + " tudo bem com você ?";
+}
+
+console.log(makeGreeting("Vanessa"));
+
+let greeting = makeGreeting(" Leandro");
+
+console.log(greeting)
+
+// outra maneira de fazer
+function makeGreeting(name) {
+    let text = " olá, " + name + " tudo bem com você ?"
+    return text;
+}
+
+// que parametro usar para saber se uma pessoa pode dirigir?
+
+function canDrive(age) {
+    if (age >= 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+let dayaneDrive = canDrive(32);
+
+console.log(dayaneDrive)
