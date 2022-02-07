333d481354d80321ee2e13fbfd9b14b85b42299e
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-07 15:46:01 -0300

    resolvido exercico 2 - para praticar

--- fundamentos/bloco4/dia4-objetos-e-funcoes/javaScript.js
+++ fundamentos/bloco4/dia4-objetos-e-funcoes/javaScript.js
@@ -14,4 +14,9 @@ let player = {
         silver: 3, 
         }
 }
-console.log(player.name+player.lastName+player.age+player.medals.golden+player.medals.silver)
+//console.log(player.name+player.lastName+player.age+player.medals.golden+player.medals.silver)
+
+// 2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console
+// uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+
+console.log(" A Jogadora" + player.name + "" + player.lastName + " tem " + player.age + " anos de idade")
