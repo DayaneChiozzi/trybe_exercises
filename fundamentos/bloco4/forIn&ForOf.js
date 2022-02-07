afbfda836e7cc0502c5d635ed445714d2664da38
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-07 20:45:41 -0300

    for in dentro de um array- videoaula

---
+++ fundamentos/bloco4/forIn&ForOf.js
@@ -0,0 +1,8 @@
+let pizzasDoces = ["chocolate", "banana", "morango"];
+for (let key in pizzasDoces) {    
+    console.log(key, pizzasDoces[key]); // => ele mostra a posição do indice + valor correspondente
+}
+
+console.log(key);// => retorna as posições do Array
+console.log(pizzasDoces); // => ele vai os valores (no caso 3 vezes por conta que são 3 posicoes)
+console.log(pizzasDoces[key]); // ele vai mostrar o indice 1 por vez
