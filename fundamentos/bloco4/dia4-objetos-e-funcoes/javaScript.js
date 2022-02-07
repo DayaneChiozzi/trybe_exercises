049a04d64da347febea7a8177697ded659ae8c4c
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-07 15:57:51 -0300

    resolvido exercicio 3 - para praticar

--- fundamentos/bloco4/dia4-objetos-e-funcoes/javaScript.js
+++ fundamentos/bloco4/dia4-objetos-e-funcoes/javaScript.js
@@ -6,17 +6,26 @@
 //let medals = { golden: 2, silver: 3 };
 
 let player = {
-    name:'Marta',
-    lastName:'Silva',
+    name: 'Marta',
+    lastName: 'Silva',
     age: 34,
-    medals:{ 
+    medals: {
         golden: 2,
-        silver: 3, 
-        }
+        silver: 3,
+    }
+        bestInTheWorld: [2006,2007,2008,2009, 2010,2018]
+        
 }
+
+
 //console.log(player.name+player.lastName+player.age+player.medals.golden+player.medals.silver)
 
 // 2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console
 // uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
-console.log(" A Jogadora " +  player.name + " " + player.lastName + " tem " + player.age + " anos de idade")
+//console.log(" A Jogadora " +  player.name + " " + player.lastName + " tem " + player.age + " anos de idade")
+
+// exercico 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo 
+// as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+console.log(player[bestInTheWorld];
