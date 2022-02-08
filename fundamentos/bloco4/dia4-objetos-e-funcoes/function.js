38a5faa57910475d5cdf5b19d8a0da7fcee2d4f2
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-08 10:59:08 -0300

    exercio funcionalidades do car-aula gravada

--- fundamentos/bloco4/dia4-objetos-e-funcoes/function.js
+++ fundamentos/bloco4/dia4-objetos-e-funcoes/function.js
@@ -48,5 +48,51 @@ let dayaneDrive = canDrive(32);
 
 console.log(dayaneDrive)
 
+// Estrutura de uma função:
 
+// function nomeDaFuncao(param1, param2)=> entrada de valores { 
+// => os paramentros serão convertidos em variaveis dentro da função.
+// return param1 + param2 => retorna um valor
+// let resultado = param1 + param2; => se necessário podemos declarar uma funcao.
+//}delimita o corpo de nossa função
+// invocar uma funcao/ chamada:
+// nomeDaFuncao(10, 20); => param1(10) param2(20) ou
+// nomeDaFuncao(); => qdo nao tem parametro
+
+// desenvolver um jogo que temos que desenvolver um carro com sua funcionalidades
+// ligar/desligar, direcionar, dar seta, acelerar, frear.
+
+//=> declarar o estado inicial do carro: 
+let statusCarro = "desligado";
+let aceleracao = 0;
+let rotacaoDoVolante = 0;
+
+function ligarDesligar() {
+    if (statusCarro === "desligado") {
+        stausCarro = "ligado";
+    } else {
+        statusCarro = "desligado"
+    }
+    return statusCarro;
+}
+
+function acelerar(incremento) {
+  aceleracao = aceleracao + incremento;
+
+  return "Acelerando a" + aceleracao + "m/s2";
+}
+
+function frear(decremento) {
+    aceleracao = aceleracao - decremento;
+
+    return "Desacelerando para" + aceleracao + "m/s2";
+
+}
+
+function girarVolante(rotacao) {
+    rotacaoDoVolante = anguloRotacao;
+
+    return rotacaoDoVolante + "Q";
+ 
+}
 
