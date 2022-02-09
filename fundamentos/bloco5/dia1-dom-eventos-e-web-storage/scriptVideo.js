d782b6fa975d8509b1381807cf8401d5c98fb375
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-09 16:09:08 -0300

    explicacao de getElementById

---
+++ fundamentos/bloco5/dia1-dom-eventos-e-web-storage/scriptVideo.js
@@ -0,0 +1,6 @@
+//console.log(document.getElementById("teste")) => imprimindo via Id
+// console.log(document.getElementById("teste").innerHTML) => imprimi todo o conteudo dessa id, paragrafo, outras tags..
+//console.log(document.getElementById("teste").innertext) => filtra tudo, imprimi apenas o texto.
+////console.log(document.getElementById("teste").style) =  imprimi todo css aplicado naquela id
+//document.getElementById("teste").innertext) = " Aprendizados da aula de hoje."; => acessando essa id e alterando
+// o texto dela.
