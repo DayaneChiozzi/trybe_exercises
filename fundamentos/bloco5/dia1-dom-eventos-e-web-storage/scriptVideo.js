65d9beb84c16f2b6eb5c825cca366e2f12e449b3
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-10 10:28:37 -0300

    alterado title, paragrado 1 e 2.

--- fundamentos/bloco5/dia1-dom-eventos-e-web-storage/scriptVideo.js
+++ fundamentos/bloco5/dia1-dom-eventos-e-web-storage/scriptVideo.js
@@ -4,3 +4,17 @@
 ////console.log(document.getElementById("teste").style) =  imprimi todo css aplicado naquela id
 //document.getElementById("teste").innertext) = " Aprendizados da aula de hoje."; => acessando essa id e alterando
 // o texto dela.
+
+//exercicio para praticar
+const paragraph = document.getElementById("paragraph");
+const pageTitle = document.getElementById("page-title");
+const secondParagraf = document.getElementById("second-paragraph");
+
+paragraph.style.color = "red";
+paragraph.innerHTML = "Filme de comédia romantica";
+paragraph.style.fontSize = "30px";
+
+pageTitle.innerHTML = "O Casamento do Meu melhor amigo";
+pageTitle.style.fontSize = "30px";
+
+secondParagraf.console.log(pageTitle);
