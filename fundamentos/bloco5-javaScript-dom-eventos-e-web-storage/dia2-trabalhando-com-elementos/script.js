1c64c1b9d5ae9f7b066519c4feb01b89bf321ddb
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-10 22:06:59 -0300

    incluido mais 4 exercicos de pratica

--- fundamentos/bloco5-javaScript-dom-eventos-e-web-storage/dia2-trabalhando-com-elementos/script.js
+++ fundamentos/bloco5-javaScript-dom-eventos-e-web-storage/dia2-trabalhando-com-elementos/script.js
@@ -20,13 +20,13 @@ const test1 = document.getElementById("pai").children[0];
 document.querySelector("#elementoOndeVoceEsta").parentNode.children[0];
 
 //6-Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
-let test2 = (document.getElementById(
-  "elementoOndeVoceEsta"
-).parentNode.nextSibling.nodeName = "#text");
+// 1º eu trouxe o elemento, elementoOndeVoceEsta e coloquei dentro de uma variavel
+// depois eu acessei o pai dele, depois o 1º elemento filho, depois o próximo elemento e
+//por fim o próximo nó.
+let test2 = document.getElementById("elementoOndeVoceEsta").parentNode
+  .firstElementChild.nextElementSibling.nextSibling;
 
-console.log(test2);
-
-//console.log(test2); não consegui
+//console.log(test2);
 
 //7-Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
 
@@ -34,6 +34,38 @@ let test3 = document.getElementById("elementoOndeVoceEsta").parentNode
   .children[2];
 //okay
 
+//console.log(test3);
 //8-Agora acesse o terceiroFilho a partir de pai .
 let test4 = document.getElementById("pai").children[2];
 // okay
+
+//1-Crie um irmão para elementoOndeVoceEsta .
+let father = document.getElementById("pai");
+let test5 = document.createElement("section");
+test5.id = "irmaoElementoOndeVoceEsta";
+
+father.appendChild(test5);
+
+console.log(father); //okay
+
+//2-Crie um filho para elementoOndeVoceEsta .
+
+let filhoElemento = document.createElement("section");
+filhoElemento.id = "elementoOndeVoceEstavaFilho";
+
+paiElement.appendChild(filhoElemento);
+
+//console.log(paiElement);okay
+
+//3-Crie um filho para primeiroFilhoDoFilho .
+let test6 = document.getElementById("primeiroFilhoDoFilho");
+let neto = document.createElement("section");
+neto.id = "primeiroFilhoDoFilho2";
+
+test6.appendChild(neto);
+
+//console.log(test6);
+
+//4-A partir desse filho criado, acesse terceiroFilho .
+
+document.getElementById("PrimeiroFilhoDoFilho2");
