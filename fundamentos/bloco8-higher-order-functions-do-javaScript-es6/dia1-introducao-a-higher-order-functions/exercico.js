5541d366c25741090c8a24f9f014360139497b4a
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-09 18:52:14 -0300

    exercicio 2

--- fundamentos/bloco8-higher-order-functions-do-javaScript-es6/dia1-introducao-a-higher-order-functions/exercico.js
+++ fundamentos/bloco8-higher-order-functions-do-javaScript-es6/dia1-introducao-a-higher-order-functions/exercico.js
@@ -13,3 +13,22 @@ const generalEmployees = (fullName) => {
 };
 
 console.log(newEmployees(generalEmployees));
+
+// para executar esse exercicio precisei da ajuda do gabarito
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio.
+//  Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e
+//  uma função que checa se o número apostado é igual ao número sorteado.
+//  O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const checkerNumber = (numberApostado, numberSorteado) =>
+  numberApostado === numberSorteado;
+
+const resultSorteio = (numberApostado, checkerNumber) => {
+  const numberSorteado = Math.floor(Math.random() * 5 + 1);
+
+  return checkerNumber(numberApostado, numberSorteado)
+    ? 'Lucky day,you won!'
+    : 'Try Again';
+};
+
+console.log(resultSorteio(5, checkerNumber));
