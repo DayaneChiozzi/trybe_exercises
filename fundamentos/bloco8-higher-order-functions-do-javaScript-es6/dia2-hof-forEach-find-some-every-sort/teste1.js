5e0fb3e9e8e9109f8e4a440e71afb63e45300d02
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-10 16:34:02 -0300

    teste de pratica conteudo do dia 8.2

---
+++ fundamentos/bloco8-higher-order-functions-do-javaScript-es6/dia2-hof-forEach-find-some-every-sort/teste1.js
@@ -0,0 +1,22 @@
+// forEach - para fixar
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+emailListInData.forEach(showEmailList);
+
+// find - para fixar
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = numbers.find((number) => {
+  return number % 3 === 0 && number % 5 === 0;
+});
+
+console.log(findDivisibleBy3And5);
